10d74ba5c9829800979f414aee3f878a8456c7ba
abhishek <abhijsh61@gmail.com>
2023-01-27 16:59:55 +0530

    second commit

--- react_project/react/serializers.py
+++ react_project/react/serializers.py
@@ -1,5 +1,6 @@
 from . models import Income, IncomeCategory, ExpenseCategory, Expense
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class IncomeCategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,23 @@ class ExpenseCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ExpenseCategory
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    # def create_user(self, validated_data):
+    #     import pdb; pdb.set_trace()
+    #     user = User.objects.create(
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #         first_name=validated_data['first_name'],
+    #         last_name=validated_data['last_name']
+    #     )
+    #
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #
+    #     return user
--- react_project/react/views.py
+++ react_project/react/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+import json
 from rest_framework.views import APIView
 from .models import Income, IncomeCategory, ExpenseCategory, Expense
-from .serializers import IncomeCategorySerializer, ExpenseCategorySerializer
+from .serializers import IncomeCategorySerializer, ExpenseCategorySerializer, UserSerializer
 from rest_framework.response import Response
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 class IncomeCategoryView(APIView):
     def get(self, request, pk=None):
+        output = {}
         income_category = IncomeCategory.objects.all()
         serializer = IncomeCategorySerializer(income_category, many=True)
+        # res = JsonResponse(serializer.data, safe=False)
+        # output['data'] = json.loads(res.content)
+        # headers = {'Access-Control-Allow-Origin': "*", 'Accept': '*/*'}
         return Response(serializer.data)
+        # return Response(serializer.data)
 
 
 class ExpenseCategoryView(APIView):
@@ -17,3 +25,22 @@ class ExpenseCategoryView(APIView):
         expense_category = ExpenseCategory.objects.all()
         serializer = ExpenseCategorySerializer(expense_category, many=True)
         return Response(serializer.data)
+
+class UserView(APIView):
+    # def get(self, request, pk=None):
+    #     users = User.objects.all()
+    #     serializer = UserSerializer(users, many=True)
+    #     return Response(serializer.data)
+
+    def post(self, request, pk=None):
+        password = request.data.get('password')
+        confirm_password = request.data.get('confirm_password')
+        if password != confirm_password:
+            return Response({'error': 'Password did not match'})
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user_obj = serializer.save()
+            user_obj.set_password(user_obj.password)
+            user_obj.save()
+            return Response({'message': 'User Created Successfully'})
+        return Response(serializer.errors)
