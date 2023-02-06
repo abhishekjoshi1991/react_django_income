7dbbe60b308e1081ea1f72c43e14989774baed5a
“abhishek” <abhijsh61@gmail.com>
2023-02-06 19:08:09 +0530

    changes in setting

--- react_project/react_project/settings.py
+++ react_project/react_project/settings.py
@@ -131,8 +131,8 @@ CORS_ORIGIN_ALLOW_ALL = False # If this is used then `CORS_ORIGIN_WHITELIST` wil
 # CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-       'http://localhost:3000',
+       'http://localhost:3000', 'http://65.0.132.5:8000/',
 )
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'http://localhost:3000', 'http://65.0.132.5:8000/',
 ]
